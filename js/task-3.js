8aed3d78c0460773f61fa0ce5e914477fb31491f
Anastasiia Pustova <anastasiia.pustova@web.de>
2024-07-09 19:37:29 +0200

    done

--- js/task-3.js
+++ js/task-3.js
@@ -9,14 +9,14 @@ const profile = {
         this.playTime += hours;
     },
     getInfo() {
-        return `${this.username} has ${this.playЕime} active hours!`;
+        return `${this.username} has ${this.playTime} active hours!`;
     }
 };
 
-// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-// profile.changeUsername("Marco");
-// console.log(profile.getInfo()); // "Marco has 300 active hours!"
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-// profile.updatePlayTime(20);
-// console.log(profile.getInfo()); // "Marco has 320 active hours!"
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
